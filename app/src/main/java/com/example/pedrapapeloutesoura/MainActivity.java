b4138bb4cef252766d8fbb5016ada1645f164c72
Nathan Lima <nathan.o.aguiar@gmail.com>
2020-11-19 21:04:21 -0300

    Parte visual concluída

---
+++ app/src/main/java/com/example/pedrapapeloutesoura/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.pedrapapeloutesoura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void selecionarPedra(View view){
+
+
+    }
+
+
+    public void opcaoSelecionada(String opcaoSelecionada){
+
+
+    }
+}
