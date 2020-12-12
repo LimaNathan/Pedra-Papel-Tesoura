ee7a11d62fd6f3a39b55b79e81e1ac7ef7e68cb8
Nathan Lima <nathan.o.aguiar@gmail.com>
2020-12-12 15:57:08 -0300

    Testando metodo para escolha do usuario

--- app/src/main/java/com/example/pedrapapeloutesoura/MainActivity.java
+++ app/src/main/java/com/example/pedrapapeloutesoura/MainActivity.java
@@ -12,14 +12,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
     public void selecionarPedra(View view){
+        this.opcaoSelecionada("Pedra");
+    }
 
+    public void selecionarPapel(View view){
+        this.opcaoSelecionada("Papel");
+    }
 
+    public void selecionarTesoura(View view){
+        this.opcaoSelecionada("Tesoura");
     }
 
 
     public void opcaoSelecionada(String opcaoSelecionada){
 
+        System.out.println("Item selecionado: " + opcaoSelecionada);
 
     }
 }
